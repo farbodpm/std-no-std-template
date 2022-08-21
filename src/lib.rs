ffca3b95073209dadebe3b8ecc0621cda97ff744
FarbodPM <farbod.peimanzadeh@gmail.com>
2022-08-21 12:49:56 +0430

    change features to improve the system

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
-#![cfg_attr(feature = "no-std", no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![crate_name = "{{name}}"]
 #![crate_type = "rlib"]
 
-#[cfg(feature = "no-std")]
 extern crate alloc;
-#[cfg(feature = "no-std")]
 use alloc::{boxed::Box, format, vec, vec::Vec};
-
-#[cfg(not(feature = "no-std"))]
-use std::{boxed::Box, format, vec, vec::Vec};
